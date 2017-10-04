39da8fc22dce110009ba46ebabd5c0bcdb806ac5
Austin Hwang <ahwang2008@gmail.com>
2017-10-04 16:44:46 -0600

    Austin's push

--- sharedlib/src/main/java/commandData/Command.java
+++ sharedlib/src/main/java/commandData/Command.java
@@ -6,10 +6,19 @@ package commandData;
 
 public class Command {
     private String type;
+    private Object data;
 
     public String getType() {
         return type;
     }
 
     public void setType(String type) { this.type = type; }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
