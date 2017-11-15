4cc5a530999ba4e24d74f3677d5c9bf7ff739175
Tarthir <tylerbrady34@gmail.com>
2017-11-15 09:44:57 -0700

    Adding on to longest route calculator.WORKS!
    Their is a unit test in the app module if you want to see

--- app/src/test/java/teamjapannumbahone/tickettoridetest/LongestRouteCalcTest.java
+++ app/src/test/java/teamjapannumbahone/tickettoridetest/LongestRouteCalcTest.java
@@ -104,35 +104,6 @@ public class LongestRouteCalcTest {
         Assert.assertEquals(calc.findLongestRoute(players),player);
 
     }
-    @Test
-    public void TestLargeGraphDistance(){
-
-        List<Player> players = new ArrayList<>();
-        Player player = new Player("Name","Name","Yellow");
-
-        player.addRoute(new Route("Calgary", "Winnipeg", "white", 6));
-        player.addRoute(new Route("Sault St. Marie", "Toronto", "Wild", 2));
-        player.addRoute(new Route("Sault St. Marie", "Winnipeg", "Wild", 6));
-        player.addRoute(new Route("Duluth", "Toronto", "purple", 6));
-        player.addRoute(new Route("Duluth", "Helena", "orange", 6));
-
-        player.addRoute(new Route("Helena", "Salt Lake City", "purple", 3));
-        player.addRoute(new Route("Portland", "Salt Lake City", "blue", 6));
-        player.addRoute(new Route("Portland", "Seattle", "Wild", 1, true));
-        player.addRoute(new Route("Seattle", "Vancouver", "Wild", 1, true));
-        player.addRoute(new Route("Calgary", "Vancouver", "Wild", 3));
-
-        players.add(player);
-        try {
-            setRouteList.invoke(calc,players.get(0).getRoutes());
-            Assert.assertEquals(40,initLongestPath.invoke(calc));
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-
-    }
 
     @Test
     public void TestLargeGraphCircle(){
