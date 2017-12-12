f9712e1b63538ea70baf1c4add66c5e276677c15
Shawn <shawn.li.cp@gmail.com>
2017-12-12 14:34:36 -0700

    client is able to be back up

--- server/src/main/java/ServerModel/Loader.java
+++ server/src/main/java/ServerModel/Loader.java
@@ -1,6 +1,7 @@
 package ServerModel;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -51,21 +52,24 @@ public class Loader {
 
     public Object loadClass(String fileName,String className) {
         String jarPath = null;
+         File myjar = null;
         if(fileName.equals("sql.txt")){
-            jarPath = "/plugins/src/main/SQL.jar";
+            myjar = new File("/plugins/src/main/SQL.jar");
+            jarPath = "./plugins/src/main/SQL.jar";
         }
         else if(fileName.equals("file.txt")){
-            jarPath = "/plugins/src/main/File.jar";
+            myjar = new File("/plugins/src/main/File.jar");
+            jarPath = "./plugins/src/main/File.jar";
         }
         // Getting the jar URL which contains target class
         try {
-            URL[] classLoaderUrls = new URL[]{new URL(jarPath)};
+            URL[] classLoaderUrls = new URL[]{myjar.toURL()};
 
             // Create a new URLClassLoader
             URLClassLoader urlClassLoader = new URLClassLoader(classLoaderUrls);
 
             // Load the target class
-            Class<?> beanClass = urlClassLoader.loadClass(className);
+            Class<?> beanClass = urlClassLoader.loadClass(className+".class");
 
             // Create a new instance from the loaded class
             return beanClass.getConstructor().newInstance();
