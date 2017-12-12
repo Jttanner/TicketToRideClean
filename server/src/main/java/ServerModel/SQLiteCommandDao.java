e3f52ebeffe0f6e4a304405820be3f5161a7fad2
jonttanner <jonttanner@gmail.com>
2017-12-12 14:02:57 -0700

    dao closing connections

--- server/src/main/java/ServerModel/SQLiteCommandDao.java
+++ server/src/main/java/ServerModel/SQLiteCommandDao.java
@@ -3,6 +3,7 @@ package ServerModel;
 import com.google.gson.Gson;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
