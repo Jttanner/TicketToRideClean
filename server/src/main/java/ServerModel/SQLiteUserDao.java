6f98d8e370ed0a407da6e80362dcd5c0a2322f2c
jonttanner <jonttanner@gmail.com>
2017-12-12 15:49:31 -0700

    UserDao tested and is nice

--- server/src/main/java/ServerModel/SQLiteUserDao.java
+++ server/src/main/java/ServerModel/SQLiteUserDao.java
@@ -25,7 +25,7 @@ public class SQLiteUserDao implements IUserDao {
         String query = "create table if not exists User \n" +
                 "( \n" +
                 "Username text unique not null, \n" +
-                "Password text not null, \n" +
+                "Password text not null \n" +
                 ");";
         try{
             connection = DriverManager.getConnection(connectionString);
@@ -41,17 +41,18 @@ public class SQLiteUserDao implements IUserDao {
     public boolean registerUser(String userName, String password) {
         String query = "INSERT INTO User(Username, Password) VALUES(?, ?)";
         try{
-            connection = DriverManager.getConnection(connectionString);
+
             if (verifyUser(userName, password) != null){
-                connection.close();
+                //connection.close();
                 return false;
             } else{
+                connection = DriverManager.getConnection(connectionString);
                 PreparedStatement queryStatement = connection.prepareStatement(query);
                 queryStatement.setString(1, userName);
-                queryStatement.setString(2, userName);
-                boolean success = queryStatement.execute();
+                queryStatement.setString(2, password);
+                queryStatement.execute();
                 connection.close();
-                return success;
+                return true;
             }
         }catch (Exception e){
             e.printStackTrace();
@@ -61,13 +62,13 @@ public class SQLiteUserDao implements IUserDao {
 
     @Override
     public User verifyUser(String name, String password) {
-        String query = "SELECT * FROM User WHERE Username=? AND Password=?";
+        String query = "SELECT * FROM User WHERE Username=? AND Password=?;";
         try{
             connection = DriverManager.getConnection(connectionString);
             PreparedStatement queryStatement = connection.prepareStatement(query);
             queryStatement.setString(1, name);
             queryStatement.setString(2, password);
-            ResultSet resultSet = queryStatement.executeQuery(query);
+            ResultSet resultSet = queryStatement.executeQuery();
             //get data to build User object from resultSet
             UserInfo foundUserInfo = new UserInfo(resultSet.getString("Username"), resultSet.getString("Password"));
             User foundUser = new User(foundUserInfo);
@@ -81,7 +82,7 @@ public class SQLiteUserDao implements IUserDao {
 
     @Override
     public boolean clear() {
-        String query = "DELETE * FROM User";
+        String query = "DELETE FROM User";
         try{
             connection = DriverManager.getConnection(connectionString);
             PreparedStatement queryStatement = connection.prepareStatement(query);
--- server/src/test/java/ServerModel/SQLiteUserDaoTest.java
+++ server/src/test/java/ServerModel/SQLiteUserDaoTest.java
@@ -5,41 +5,52 @@ import org.junit.Assert;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import modeling.User;
+import modeling.UserInfo;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by jontt on 12/7/2017.
  */
 public class SQLiteUserDaoTest {
-    Connection connection;
-    String dbURL = "jdbc:sqlite:c/Users/jontt/AndroidStudioProjects/TicketToRidePhase3/TicketToRidePhase3Testing/test.db";
+    //Connection connection;
+    String dbURL = "jdbc:sqlite:ttrdb.sqlite";
     SQLiteUserDao userDao;
+
+    String testName = "TestName";
+    String testPassword = "TestPassword";
 
     @org.junit.Before
     public void setUp() throws Exception {
-        connection = DriverManager.getConnection(dbURL);
-        userDao = new SQLiteUserDao(connection);
+        Class.forName("org.sqlite.JDBC");
+        userDao = new SQLiteUserDao(dbURL);
     }
 
     @org.junit.After
     public void tearDown() throws Exception {
-        connection.close();
         userDao = null;
     }
 
     @org.junit.Test
     public void registerUser() throws Exception {
+        userDao.clear();
         Assert.assertTrue(userDao.registerUser("TestName", "TestPassword"));
     }
 
     @org.junit.Test
     public void verifyUser() throws Exception {
-
+        userDao.clear();
+        userDao.registerUser(testName, testPassword);
+        UserInfo testUserInfo = new User(new UserInfo(testName, testPassword)).getInfo();
+        UserInfo foundUserInfo = userDao.verifyUser(testName, testPassword).getInfo();
+        Assert.assertTrue(testUserInfo.getUserName().equals(foundUserInfo.getUserName()));
+        Assert.assertTrue(testUserInfo.getPassword().equals(foundUserInfo.getPassword()));
     }
 
     @org.junit.Test
     public void clear() throws Exception {
-
+        userDao.clear();
     }
 
 }
